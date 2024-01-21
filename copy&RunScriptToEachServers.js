089b03b979e49782c4ec773b087d4218b31d9219
hilawnabangus <jaymaryugo90@gmail.com>
2024-01-21 16:00:37 +0800

    Update copy&RunScriptToEachServers.js
    major update

--- copy&RunScriptToEachServers.js
+++ copy&RunScriptToEachServers.js
@@ -1,22 +1,21 @@
 /** @param {NS} ns */
+function dpList(ns, current = "home", set = new Set()) 
+{
+    let connections = ns.scan(current);
+    let next = connections.filter(c => !set.has(c));
+    next.forEach(n => 
+    {
+        set.add(n); return dpList(ns, n, set);
+    });
+    return Array.from(set.keys());
+}
 export async function main(ns) {
-    //create array of servers
-    let serversSeen = ['home']
-    for (let i = 0; i < serversSeen.length; i++) {
-        let thisScan = ns.scan(serversSeen[i]);
-        for (let j = 0; j < thisScan.length; j++) {
-            // ns.print(thisScan);
-            if (serversSeen.indexOf(thisScan[j]) === -1) {
-                serversSeen.push(thisScan[j]);
-            }
-        }
-    }
-
     //copy and run script to each available server
+    let availableServers = dpList(ns);
     const taskName = 'early-hack-template.js';
 
-    for (let i = 0; i < serversSeen.length; ++i) {
-        const serv = serversSeen[i];
+    for (let i = 0; i < availableServers.length; ++i) {
+        const serv = availableServers[i];
         if (!ns.fileExists(taskName)) {
             let threadCountNeeded = Math.floor((ns.getServerMaxRam(serv) - ns.getServerUsedRam(serv)) / ns.getScriptRam(taskName));
 
