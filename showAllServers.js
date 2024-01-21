377006792f756280ca7072d672d194872b83ebd4
hilawnabangus <jaymaryugo90@gmail.com>
2024-01-21 15:16:11 +0800

    Create showAllServers.js
    scan all available server

---
+++ showAllServers.js
@@ -0,0 +1,14 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    let serversSeen = ['home']
+      for (let i = 0; i < serversSeen.length; i++) {
+        let thisScan = ns.scan(serversSeen[i]);
+        for (let j = 0; j < thisScan.length; j++) {
+          // ns.print(thisScan);
+          if (serversSeen.indexOf(thisScan[j]) === -1) {
+            serversSeen.push(thisScan[j]);
+          }
+        }
+      }
+      return serversSeen;
+  }
