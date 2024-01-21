2c22a0a4a2a4dd5efb24da0c29e34f5f3813d54c
hilawnabangus <jaymaryugo90@gmail.com>
2024-01-21 15:58:56 +0800

    Update copy&RunScriptToEachServers.js
    update

--- copy&RunScriptToEachServers.js
+++ copy&RunScriptToEachServers.js
@@ -12,19 +12,19 @@ export async function main(ns) {
         }
     }
 
-    //
+    //copy and run script to each available server
     const taskName = 'early-hack-template.js';
 
-    for(let i = 0; i<serversSeen.length; i++){
-        if (!ns.fileExists("early-hack-template.js")) {
-            const serv = serversSeen[i];
-            let taskThreadCount = Math.floor((ns.getServerMaxRam(serv) - ns.getServerUsedRam(serv)) / ns.getScriptRam(taskName));
-            
-            ns.scp(taskName,serv);
+    for (let i = 0; i < serversSeen.length; ++i) {
+        const serv = serversSeen[i];
+        if (!ns.fileExists(taskName)) {
+            let threadCountNeeded = Math.floor((ns.getServerMaxRam(serv) - ns.getServerUsedRam(serv)) / ns.getScriptRam(taskName));
+
+            ns.scp(taskName, serv);
             ns.brutessh(serv);
             ns.nuke(serv);
-            ns.exec(taskName,serv,taskThreadCount);
-            
+            ns.exec(taskName, serv, threadCountNeeded);
+
         }
     }
 
