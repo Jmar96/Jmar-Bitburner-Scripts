dfe3ffb16b9b379e27c72bdf09e981b8e515f35d
hilawnabangus <jaymaryugo90@gmail.com>
2024-01-25 10:59:07 +0800

    Update copy&RunScriptToEachServers.js
    update, add thread condition

--- copy&RunScriptToEachServers.js
+++ copy&RunScriptToEachServers.js
@@ -4,55 +4,58 @@ function dpList(ns, current = "home", set = new Set()) {
     let connections = ns.scan(current);
     let next = connections.filter(c => !set.has(c));
     next.forEach(n => {
-        set.add(n); return dpList(ns, n, set);
+      set.add(n); return dpList(ns, n, set);
     });
     return Array.from(set.keys());
-}
-export async function main(ns) {
+  }
+  export async function main(ns) {
     //copy and run script to each available server
     let availableServers = dpList(ns);
     const taskName = "early-hack-template.js";
-
+  
     for (let i = 0; i < availableServers.length; ++i) {
-        const serv = availableServers[i];
-        let threadCountNeeded = Math.floor((ns.getServerMaxRam(serv) - ns.getServerUsedRam(serv)) / ns.getScriptRam(taskName));
-
-        if (ns.getServerRequiredHackingLevel(serv) < ns.getHackingLevel()) {
-            if (!ns.fileExists(taskName, serv)) {
-
-                ns.tprint("NoExist (will create) | Threads available: " + threadCountNeeded + " | " + serv);
-
-                ns.scp(taskName, serv);
-                // ns.brutessh(serv);
-                if (ns.fileExists("BruteSSH.exe", "home")) {
-                    await ns.brutessh(serv);
-                }
-                if (ns.fileExists("FTPCrack.exe", "home")) {
-                    await ns.ftpcrack(serv);
-                }
-                if (ns.fileExists("relaySMTP.exe", "home")) {
-                    await ns.relaysmtp(serv);
-                }
-                if (ns.fileExists("HTTPWorm.exe", "home")) {
-                    await ns.httpworm(serv);
-                }
-                if (ns.fileExists("SQLInject.exe", "home")) {
-                    await ns.sqlinject(serv);
-                }
-                // ns.nuke(serv);
-                if (!ns.hasRootAccess(serv)) {
-                    await ns.nuke(serv);
-                }
-                if (threadCountNeeded > 0) {
-                    ns.exec(taskName, serv, threadCountNeeded);
-                }
-
-            } else {
-                ns.tprint("Exist (wont create) | Threads available: " + threadCountNeeded + " | " + serv);
+      const serv = availableServers[i];
+      let threadCountNeeded = Math.floor((ns.getServerMaxRam(serv) - ns.getServerUsedRam(serv)) / ns.getScriptRam(taskName));
+  
+      if (ns.getServerRequiredHackingLevel(serv) < ns.getHackingLevel()) {
+        if (!ns.fileExists(taskName, serv)) {
+          if (threadCountNeeded > 0) {
+  
+            ns.tprint("NotExist (will create) | Threads available: " + threadCountNeeded + " | " + serv);
+  
+            ns.scp(taskName, serv);
+            // ns.brutessh(serv);
+            if (ns.fileExists("BruteSSH.exe", "home")) {
+              await ns.brutessh(serv);
             }
+            if (ns.fileExists("FTPCrack.exe", "home")) {
+              await ns.ftpcrack(serv);
+            }
+            if (ns.fileExists("relaySMTP.exe", "home")) {
+              await ns.relaysmtp(serv);
+            }
+            if (ns.fileExists("HTTPWorm.exe", "home")) {
+              await ns.httpworm(serv);
+            }
+            if (ns.fileExists("SQLInject.exe", "home")) {
+              await ns.sqlinject(serv);
+            }
+            // ns.nuke(serv);
+            if (!ns.hasRootAccess(serv)) {
+              await ns.nuke(serv);
+            }
+            ns.exec(taskName, serv, threadCountNeeded);
+  
+          } else {
+            ns.tprint("NotExist but [O]Threads available (wont create) | Threads available: " + threadCountNeeded + " | " + serv);
+          }
+  
+        } else {
+          ns.tprint("Exist (wont create) | Threads available: " + threadCountNeeded + " | " + serv);
         }
-
+      }
+  
     }
-
-
-}
+  
+  
+  }
