521201c5a84d7649a93837f229ecf1798d8169b9
hilawnabangus <jaymaryugo90@gmail.com>
2024-01-30 18:31:12 +0800

    update
    added conditions for checking required open ports

--- checkAllAvailableServers.js
+++ checkAllAvailableServers.js
@@ -16,14 +16,44 @@ export async function main(ns) {
     for (let i = 0; i < availableServers.length; ++i) {
         const serv = availableServers[i];
         let threadCountNeeded = Math.floor((ns.getServerMaxRam(serv) - ns.getServerUsedRam(serv)) / ns.getScriptRam(taskName));
+        let svrMoneyAvailable = ns.getServerMoneyAvailable(serv);
+        let portsRequired = ns.getServerNumPortsRequired(serv);
+        let p = 0;
 
         if (ns.getServerRequiredHackingLevel(serv) > ns.getHackingLevel()) {
-            ns.tprint("WARN: Insufficient hacking skill for %s", serv);
+            ns.tprint("WARN: Insufficient hacking skill for ", serv);
         } else {
             if (!ns.fileExists(taskName, serv)) {
-                ns.tprint("NoExistScript (will create) | Threads available: " + threadCountNeeded + " | " + serv);
+                // ns.brutessh(serv);
+                if (ns.fileExists("BruteSSH.exe", "home")) {
+                    p = p + 1;
+                }
+                if (ns.fileExists("FTPCrack.exe", "home")) {
+                    p = p + 1;
+                }
+                if (ns.fileExists("relaySMTP.exe", "home")) {
+                    p = p + 1;
+                }
+                if (ns.fileExists("HTTPWorm.exe", "home")) {
+                    p = p + 1;
+                }
+                if (ns.fileExists("SQLInject.exe", "home")) {
+                    p = p + 1;
+                }
+                // if number of port mets then nuke and copy/run scripts
+                if (p >= portsRequired) {
+                    ns.tprint("INFO-NoExistScript (will create on " + serv + ") | Threads available: " + threadCountNeeded + " | Money: " + svrMoneyAvailable);
+                } else {
+                    ns.rm(taskName, serv);
+                    ns.tprint("ERROR::NotExist but |Ports (P[" + p + "] > PR[" + portsRequired + "])| (wont create & delete existing on " + serv + ") | Threads available: " + threadCountNeeded + " | Money: " + svrMoneyAvailable);
+                }
             } else {
-                ns.tprint("ScriptExist (wont create) | Threads available: " + threadCountNeeded + " | " + serv);
+                if (threadCountNeeded > ns.getScriptRam(taskName)) {
+                    ns.rm(taskName, serv);
+                    ns.tprint("ERROR::ScriptExist but not running (delete script on " + serv + ") | Threads available: " + threadCountNeeded + " | Money: " + svrMoneyAvailable);
+                } else {
+                    ns.tprint("ScriptExist (wont create " + serv + ") | Threads available: " + threadCountNeeded + " | Money: " + svrMoneyAvailable);
+                }
             }
         }
 
--- copy&RunScriptToEachServers.js
+++ copy&RunScriptToEachServers.js
@@ -1,61 +1,72 @@
 /** @param {NS} ns */
 //create a array list of avaliable servers
 function dpList(ns, current = "home", set = new Set()) {
-    let connections = ns.scan(current);
-    let next = connections.filter(c => !set.has(c));
-    next.forEach(n => {
-      set.add(n); return dpList(ns, n, set);
-    });
-    return Array.from(set.keys());
-  }
-  export async function main(ns) {
-    //copy and run script to each available server
-    let availableServers = dpList(ns);
-    const taskName = "early-hack-template.js";
-  
-    for (let i = 0; i < availableServers.length; ++i) {
-      const serv = availableServers[i];
-      let threadCountNeeded = Math.floor((ns.getServerMaxRam(serv) - ns.getServerUsedRam(serv)) / ns.getScriptRam(taskName));
-  
-      if (ns.getServerRequiredHackingLevel(serv) < ns.getHackingLevel()) {
-        if (!ns.fileExists(taskName, serv)) {
-          if (threadCountNeeded > 0) {
-  
+  let connections = ns.scan(current);
+  let next = connections.filter(c => !set.has(c));
+  next.forEach(n => {
+    set.add(n); return dpList(ns, n, set);
+  });
+  return Array.from(set.keys());
+}
+export async function main(ns) {
+  //copy and run script to each available server
+  let availableServers = dpList(ns);
+  const taskName = "early-hack-template.js";
+
+  for (let i = 0; i < availableServers.length; ++i) {
+    const serv = availableServers[i];
+    let threadCountNeeded = Math.floor((ns.getServerMaxRam(serv) - ns.getServerUsedRam(serv)) / ns.getScriptRam(taskName));
+    let portsRequired = ns.getServerNumPortsRequired(serv);
+    let p = 0;
+
+    if (ns.getServerRequiredHackingLevel(serv) < ns.getHackingLevel()) {
+      if (!ns.fileExists(taskName, serv)) {
+        if (threadCountNeeded > 0) {
+
+          // ns.brutessh(serv);
+          if (ns.fileExists("BruteSSH.exe", "home")) {
+            await ns.brutessh(serv);
+            p = p + 1;
+          }
+          if (ns.fileExists("FTPCrack.exe", "home")) {
+            await ns.ftpcrack(serv);
+            p = p + 1;
+          }
+          if (ns.fileExists("relaySMTP.exe", "home")) {
+            await ns.relaysmtp(serv);
+            p = p + 1;
+          }
+          if (ns.fileExists("HTTPWorm.exe", "home")) {
+            await ns.httpworm(serv);
+            p = p + 1;
+          }
+          if (ns.fileExists("SQLInject.exe", "home")) {
+            await ns.sqlinject(serv);
+            p = p + 1;
+          }
+          // if number of port mets then nuke and copy/run scripts
+          if (p >= portsRequired) {
             ns.tprint("NotExist (will create) | Threads available: " + threadCountNeeded + " | " + serv);
-  
             ns.scp(taskName, serv);
-            // ns.brutessh(serv);
-            if (ns.fileExists("BruteSSH.exe", "home")) {
-              await ns.brutessh(serv);
-            }
-            if (ns.fileExists("FTPCrack.exe", "home")) {
-              await ns.ftpcrack(serv);
-            }
-            if (ns.fileExists("relaySMTP.exe", "home")) {
-              await ns.relaysmtp(serv);
-            }
-            if (ns.fileExists("HTTPWorm.exe", "home")) {
-              await ns.httpworm(serv);
-            }
-            if (ns.fileExists("SQLInject.exe", "home")) {
-              await ns.sqlinject(serv);
-            }
             // ns.nuke(serv);
             if (!ns.hasRootAccess(serv)) {
               await ns.nuke(serv);
             }
             ns.exec(taskName, serv, threadCountNeeded);
-  
           } else {
-            ns.tprint("NotExist but [O]Threads available (wont create) | Threads available: " + threadCountNeeded + " | " + serv);
+            ns.tprint("WARN::NotExist but Ports (P[" + p + "] > PR[" + portsRequired + "])| (wont create) | Threads available: " + threadCountNeeded + " | " + serv);
           }
-  
+
         } else {
-          ns.tprint("Exist (wont create) | Threads available: " + threadCountNeeded + " | " + serv);
+          ns.tprint("WARN::NotExist but [O]Threads available (wont create) | Threads available: " + threadCountNeeded + " | " + serv);
         }
+
+      } else {
+        ns.tprint("WARN::Exist (wont create) | Threads available: " + threadCountNeeded + " | " + serv);
       }
-  
     }
-  
-  
+
   }
+
+
+}
